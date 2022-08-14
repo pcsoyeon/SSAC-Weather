67f56adcb372f9ed088cdb37d1bad2e20be26971
so yeon <alicesysy@swu.ac.kr>
2022-08-14 11:19:58 +0900

    [#1] 폴더명 변경 및 주석정리

--- Weather/Weather/Screen/Main/Controller/ViewController.swift
+++ Weather/Weather/Screen/Main/Controller/ViewController.swift
@@ -69,20 +69,20 @@ extension MainViewController {
     }
     
     func showRequestLocationServiceAlert() {
-            let requestLocationServiceAlert = UIAlertController(title: "위치정보 이용", message: "위치 서비스를 사용할 수 없습니다. 기기의 '설정 > 개인정보 보호'에서 위치 서비스를 켜주세요.", preferredStyle: .alert)
-            
-            let goSetting = UIAlertAction(title: "설정으로 이동", style: .destructive) { _ in
-                if let appSetting = URL(string: UIApplication.openSettingsURLString) {
-                    UIApplication.shared.open(appSetting)
-                }
+        let requestLocationServiceAlert = UIAlertController(title: "위치정보 이용", message: "위치 서비스를 사용할 수 없습니다. 기기의 '설정 > 개인정보 보호'에서 위치 서비스를 켜주세요.", preferredStyle: .alert)
+        
+        let goSetting = UIAlertAction(title: "설정으로 이동", style: .destructive) { _ in
+            if let appSetting = URL(string: UIApplication.openSettingsURLString) {
+                UIApplication.shared.open(appSetting)
             }
-            
-            let cancel = UIAlertAction(title: "취소", style: .default)
-            requestLocationServiceAlert.addAction(cancel)
-            requestLocationServiceAlert.addAction(goSetting)
-            
-            present(requestLocationServiceAlert, animated: true, completion: nil)
         }
+        
+        let cancel = UIAlertAction(title: "취소", style: .default)
+        requestLocationServiceAlert.addAction(cancel)
+        requestLocationServiceAlert.addAction(goSetting)
+        
+        present(requestLocationServiceAlert, animated: true, completion: nil)
+    }
 }
 
 // MARK: - CLLocation Protocol
