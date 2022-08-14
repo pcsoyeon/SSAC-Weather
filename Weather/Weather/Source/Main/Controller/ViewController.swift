72e24209ac2c6b8f5b411a70481612183ee7509f
so yeon <alicesysy@swu.ac.kr>
2022-08-14 11:19:00 +0900

    [#2] 사용자 현재 위치 확인

--- Weather/Weather/Source/Main/Controller/ViewController.swift
+++ Weather/Weather/Source/Main/Controller/ViewController.swift
@@ -6,12 +6,104 @@
 //
 
 import UIKit
+import CoreLocation
 
 class MainViewController: UIViewController {
-
+    
+    // MARK: - Property
+    
+    private let locationManager = CLLocationManager()
+    
+    private var latitude = CLLocationCoordinate2D().latitude
+    private var longtitude = CLLocationCoordinate2D().longitude
+    
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setLocationManager()
     }
-
+    
+    // MARK: - Custom Method
+    
+    private func setLocationManager() {
+        locationManager.delegate = self
+    }
 }
 
+// MARK: - Authorization
+
+extension MainViewController {
+    func checkUserDeviceLocationServiceAuthorization() {
+        let authorizationStatus: CLAuthorizationStatus
+        
+        if #available(iOS 14.0, *) {
+            authorizationStatus = locationManager.authorizationStatus
+        } else {
+            authorizationStatus = CLLocationManager.authorizationStatus()
+        }
+        
+        if CLLocationManager.locationServicesEnabled() {
+            checkUserCurrentLocationAuthorization(authorizationStatus)
+        } else {
+            print("위치 서비스가 꺼져 있어 위치 권한 요청을 하지 못합니다.")
+        }
+    }
+    
+    func checkUserCurrentLocationAuthorization(_ authorizationStatus: CLAuthorizationStatus) {
+        switch authorizationStatus {
+        case .notDetermined:
+            print("NOT DETERMINED")
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.startUpdatingLocation()
+        case .restricted, .denied:
+            print("DENIED, 아이폰 설정으로 유도")
+            showRequestLocationServiceAlert()
+        case .authorizedWhenInUse:
+            print("WHEN IN USE")
+            locationManager.startUpdatingLocation()
+        default:
+            print("DEFAULT")
+        }
+    }
+    
+    func showRequestLocationServiceAlert() {
+            let requestLocationServiceAlert = UIAlertController(title: "위치정보 이용", message: "위치 서비스를 사용할 수 없습니다. 기기의 '설정 > 개인정보 보호'에서 위치 서비스를 켜주세요.", preferredStyle: .alert)
+            
+            let goSetting = UIAlertAction(title: "설정으로 이동", style: .destructive) { _ in
+                if let appSetting = URL(string: UIApplication.openSettingsURLString) {
+                    UIApplication.shared.open(appSetting)
+                }
+            }
+            
+            let cancel = UIAlertAction(title: "취소", style: .default)
+            requestLocationServiceAlert.addAction(cancel)
+            requestLocationServiceAlert.addAction(goSetting)
+            
+            present(requestLocationServiceAlert, animated: true, completion: nil)
+        }
+}
+
+// MARK: - CLLocation Protocol
+
+extension MainViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(#function, locations)
+        
+        if let coordinate = locations.last?.coordinate {
+            latitude = coordinate.latitude
+            longtitude = coordinate.longitude
+        }
+        locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(#function, error)
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        print(#function)
+        checkUserDeviceLocationServiceAuthorization()
+    }
+}
