bf939a1b0f6b99fc4ba8b676e0596bdffaa860ea
so yeon <alicesysy@swu.ac.kr>
2022-08-16 08:03:28 +0900

    [#5] Air Pollution Response Model 추가

---
+++ Weather/Weather/Screen/Main/Model/AirPollutionResponse.swift
@@ -0,0 +1,19 @@
+//
+//  AirPollutionRespones.swift
+//  Weather
+//
+//  Created by 소연 on 2022/08/16.
+//
+
+import Foundation
+
+struct AirPollutionResponse {
+    let co: Double
+    let no: Double
+    let no2: Double
+    let o3: Double
+    let so2: Double
+    let pm2_5: Double
+    let pm10: Double
+    let nh3: Double
+}
